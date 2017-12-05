297fcc4a93bb362bc73d681bd28a3fc33a30addc
Armando Collazo Amable <acll19@gmail.com>
2017-12-05 01:03:08 -0500

    Some fixes before kicking of on ubuntu

--- src/main/java/org/aca/studies/domain/services/EmployeeService.java
+++ src/main/java/org/aca/studies/domain/services/EmployeeService.java
@@ -2,6 +2,7 @@ package org.aca.studies.domain.services;
 
 import org.aca.studies.domain.Employee;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,11 +11,13 @@ public class EmployeeService {
     public List<Employee> listEmployees() {
         return Arrays.asList(
                 new Employee.Builder()
-                .withName("Employee A")
-                .build(),
+                        .withName("Employee A")
+                        .withSallary(BigDecimal.valueOf(1500.0))
+                        .build(),
                 new Employee.Builder()
-                .withName("Employee B")
-                .build()
+                        .withName("Employee B")
+                        .withSallary(BigDecimal.valueOf(1400.0))
+                        .build()
         );
     }
 }
